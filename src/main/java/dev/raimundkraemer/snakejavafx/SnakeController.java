9384eb929e8171dcd031e7318a5efcc3d9dae386
Raimund Kraemer <kraemer_raimund@web.de>
2023-11-26 20:10:47 +0100

    Rename HelloController to SnakeController

--- src/main/java/dev/raimundkraemer/snakejavafx/SnakeController.java
+++ src/main/java/dev/raimundkraemer/snakejavafx/SnakeController.java
@@ -3,7 +3,7 @@ package dev.raimundkraemer.snakejavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SnakeController {
     @FXML
     private Label welcomeText;
 
