f5d63b38130f72148517e9ea6c39b3d866760aea
Raimund Kraemer <kraemer_raimund@web.de>
2023-11-27 19:21:01 +0100

    Draw rectangle to verify canvas is prepared

--- src/main/java/dev/raimundkraemer/snake/SnakeController.java
+++ src/main/java/dev/raimundkraemer/snake/SnakeController.java
@@ -2,9 +2,16 @@ package dev.raimundkraemer.snake;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 
 public class SnakeController {
 
     @FXML
     Canvas canvas;
+
+    public void initialize() {
+        final var graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.setFill(Color.BLACK);
+        graphicsContext.fillRect(100, 100, 80, 40);
+    }
 }
